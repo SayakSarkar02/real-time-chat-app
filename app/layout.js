46a090637d0c329fa1101e829d761fc18e1a31c4
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-07-07 01:33:53 +0530

    new psuh

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
+      
       <body className={inter.className + " bg-black "}>{children}</body>
     </html>
   );
